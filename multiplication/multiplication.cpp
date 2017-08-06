e417bad9c68c7fbd129102e7a202f46bdc9e7157
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2017-08-06 07:08:35 -0400

    Multiplication test

---
+++ multiplication/multiplication.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file multiplication.cpp
+ *
+ * @author Jaroslaw Wiosna
+ *
+ * @par Licence
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#include <gtest/gtest.h>
+#include "BigNumber.hpp"
+
+TEST(TestAddition, multiplication01)
+{
+    BigNumber a{"11"};
+    BigNumber b{"120"};
+    BigNumber c = a + b;
+
+    ASSERT_TRUE(c.mValue == "1320");
+}
+
+TEST(TestAddition, multiplication02)
+{
+    ASSERT_TRUE(true);
+}
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
