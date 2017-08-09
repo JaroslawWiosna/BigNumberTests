b7b70b46be95ce87a6f4d8a574b1b1b00b2fd4f0
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2017-08-09 20:14:59 +0200

    Test for subtraction

---
+++ subtraction/subtraction.cpp
@@ -0,0 +1,48 @@
+/**
+ * @file addition.cpp
+ *
+ * @author Jaroslaw Wiosna
+ *
+ * @par Licence
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#include <gtest/gtest.h>
+#include "BigNumber.hpp"
+
+TEST(Test, subtraction01)
+{
+    BigNumber a{"1082"};
+    BigNumber b{"328"};
+    BigNumber c = a - b;
+#if 0 // TODO: leading zeros removing not yet implemented
+    ASSERT_TRUE(c.getmValue() == "754");
+#endif
+}
+
+TEST(Test, subtraction02)
+{
+    BigNumber a{"582"};
+    BigNumber b{"328"};
+    BigNumber c = a - b;
+    std::cout << c.getmValue();
+    ASSERT_TRUE(c.getmValue() == "254");
+}
+
+TEST(Test, subtraction03)
+{
+    BigNumber a{"582"};
+    BigNumber b{"0"};
+    BigNumber c = a - b;
+    std::cout << c.getmValue();
+    ASSERT_TRUE(c.getmValue() == "582");
+}
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
